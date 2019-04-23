0a09ba186b8f803605ff9024cce7f01c8660c236
Tyler Ilunga <tylerilunga1@gmail.com>
2019-04-23 14:24:49 -0700

    VSCode Prettier

--- classwork/pathfinder/Pathfinder.java
+++ classwork/pathfinder/Pathfinder.java
@@ -7,35 +7,36 @@ import java.util.Map;
 import java.util.Queue;
 
 /**
- * Maze Pathfinding algorithm that implements a basic, uninformed, breadth-first tree search.
+ * Maze Pathfinding algorithm that implements a basic, uninformed, breadth-first
+ * tree search.
  */
 public class Pathfinder {
 
     /**
-     * Given a MazeProblem, which specifies the actions and transitions available in the
-     * search, returns a solution to the problem as a sequence of actions that leads from
-     * the initial to a goal state.
+     * Given a MazeProblem, which specifies the actions and transitions available in
+     * the search, returns a solution to the problem as a sequence of actions that
+     * leads from the initial to a goal state.
      *
      * @param problem A MazeProblem that specifies the maze, actions, transitions.
-     * @return An ArrayList of Strings representing actions that lead from the initial to
-     * the goal state, of the format: ["R", "R", "L", ...]
+     * @return An ArrayList of Strings representing actions that lead from the
+     *         initial to the goal state, of the format: ["R", "R", "L", ...]
      */
-    public static ArrayList<String> solve (MazeProblem problem) {
+    public static ArrayList<String> solve(MazeProblem problem) {
         Queue<SearchTreeNode> frontier = new LinkedList<SearchTreeNode>();
         frontier.add(new SearchTreeNode(problem.INITIAL_STATE, null, null));
 
         while (!frontier.isEmpty()) {
-          SearchTreeNode current = frontier.poll();
-          if (problem.isGoal(current.state)) {
-              return getActionSequence(current, new ArrayList<String>());
-          }
+            SearchTreeNode current = frontier.poll();
+            if (problem.isGoal(current.state)) {
+                return getActionSequence(current, new ArrayList<String>());
+            }
 
-          Map<String, MazeState> transitions = problem.getTransitions(current.state);
-          for(Map.Entry<String, MazeState> transition : transitions.entrySet()) {
-              String action = transition.getKey();
-              MazeState nextState = transition.getValue();
-              frontier.add(new SearchTreeNode(nextState, action, current));
-          }
+            Map<String, MazeState> transitions = problem.getTransitions(current.state);
+            for (Map.Entry<String, MazeState> transition : transitions.entrySet()) {
+                String action = transition.getKey();
+                MazeState nextState = transition.getValue();
+                frontier.add(new SearchTreeNode(nextState, action, current));
+            }
         }
         return null;
     }
@@ -43,16 +44,18 @@ public class Pathfinder {
     /**
      * Returns the final list of actions that the agent took.
      *
-     * @param node A SearchTreeNode (state, action, parent) contained in final
-     * sequence.
-     * @param actions A list of strings that contain of the following actions:
-     * ("U", "D", "L", "R")
+     * @param node    A SearchTreeNode (state, action, parent) contained in final
+     *                sequence.
+     * @param actions A list of strings that contain of the following actions: ("U",
+     *                "D", "L", "R")
      * @return Boolean of whether or not the given state is a Goal.
      */
     private static ArrayList<String> getActionSequence(SearchTreeNode node, ArrayList<String> actions) {
-      if (node.parent == null) { return actions; }
-      actions.add(0, node.action);
-      return getActionSequence(node.parent, actions);
+        if (node.parent == null) {
+            return actions;
+        }
+        actions.add(0, node.action);
+        return getActionSequence(node.parent, actions);
     }
 }
 
@@ -69,11 +72,11 @@ class SearchTreeNode {
     /**
      * Constructs a new SearchTreeNode to be used in the Search Tree.
      *
-     * @param state The MazeState (col, row) that this node represents.
+     * @param state  The MazeState (col, row) that this node represents.
      * @param action The action that *led to* this state / node.
      * @param parent Reference to parent SearchTreeNode in the Search Tree.
      */
-    SearchTreeNode (MazeState state, String action, SearchTreeNode parent) {
+    SearchTreeNode(MazeState state, String action, SearchTreeNode parent) {
         this.state = state;
         this.action = action;
         this.parent = parent;
