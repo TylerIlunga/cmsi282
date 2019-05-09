b72fd89923d8c43548e70259918a29140157a26a
Tyler Ilunga <tylerilunga1@gmail.com>
2019-05-08 20:24:20 -0700

    final

--- homework/csp/CSP.java
+++ homework/csp/CSP.java
@@ -1,8 +1,9 @@
 package csp;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.time.LocalDate;
 import java.util.Set;
-import java.util.List;
 
 /**
  * CSP: Calendar Satisfaction Problem Solver Provides a solution for scheduling
@@ -30,7 +31,181 @@ public class CSP {
      */
     public static List<LocalDate> solve(int nMeetings, LocalDate rangeStart, LocalDate rangeEnd,
             Set<DateConstraint> constraints) {
-        throw new UnsupportedOperationException();
+        List<MeetingVariable> variables = populateVariables(new ArrayList<MeetingVariable>(), nMeetings, rangeStart,
+                rangeEnd);
+        handlePreprocessing(variables, constraints);
+        return handleAssignments(variables, constraints);
+    }
+
+    /** Helper Methods */
+
+    private static List<MeetingVariable> populateVariables(ArrayList<MeetingVariable> variables, int totalMeetings,
+            LocalDate startDate, LocalDate endDate) {
+        for (int i = 0; i < totalMeetings; i++) {
+            variables.add(new MeetingVariable(startDate, endDate));
+        }
+        return variables;
+    }
+
+    private static List<MeetingVariable> handlePreprocessing(List<MeetingVariable> variables,
+            Set<DateConstraint> constraints) {
+        for (MeetingVariable meeting : variables) {
+            List<LocalDate> meetingDomain = meeting.domain;
+            for (int i = 0; i < meetingDomain.size(); i++) {
+                LocalDate date = meetingDomain.get(i);
+                for (DateConstraint constraint : constraints) {
+                    if (constraint.arity() == 1) {
+                        LocalDate rightDate = ((UnaryDateConstraint) constraint).R_VAL;
+                        if (!isValidConstraint(date, constraint, rightDate)) {
+                            meetingDomain.remove(date);
+                            i--;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return variables;
+    }
+
+    private static boolean isValidConstraint(LocalDate leftDate, DateConstraint constraint, LocalDate rightDate) {
+        boolean isValid = false;
+        switch (constraint.OP) {
+        case "==":
+            if (leftDate.isEqual(rightDate))
+                isValid = true;
+            break;
+        case "!=":
+            if (!leftDate.isEqual(rightDate))
+                isValid = true;
+            break;
+        case ">":
+            if (leftDate.isAfter(rightDate))
+                isValid = true;
+            break;
+        case "<":
+            if (leftDate.isBefore(rightDate))
+                isValid = true;
+            break;
+        case ">=":
+            if (leftDate.isAfter(rightDate) || leftDate.isEqual(rightDate))
+                isValid = true;
+            break;
+        case "<=":
+            if (leftDate.isBefore(rightDate) || leftDate.isEqual(rightDate))
+                isValid = true;
+            break;
+        }
+        return isValid;
+    }
+
+    private static List<LocalDate> gatherCompleteAssignment(List<LocalDate> completeAssignment,
+            List<MeetingVariable> variables) {
+        for (MeetingVariable meeting : variables) {
+            completeAssignment.add(meeting.getAssignment());
+        }
+        return completeAssignment;
+    }
+
+    private static List<LocalDate> handleAssignments(List<MeetingVariable> variables, Set<DateConstraint> constraints) {
+        if (variables.get(variables.size() - 1).getAssignment() != null) {
+            return gatherCompleteAssignment(new ArrayList<LocalDate>(), variables);
+        }
+        MeetingVariable current = getCurrentMeeting(variables);
+        for (LocalDate date : current.domain) {
+            current.setAssignment(date);
+            if (checkConsistency(variables, constraints)) {
+                List<LocalDate> completeAssignment = handleAssignments(variables, constraints);
+                if (completeAssignment != null) {
+                    return completeAssignment;
+                }
+            }
+            current.removeAssignment();
+        }
+        return null;
+    }
+
+    private static MeetingVariable getCurrentMeeting(List<MeetingVariable> variables) {
+        for (MeetingVariable meeting : variables) {
+            if (meeting.getAssignment() == null) {
+                return meeting;
+            }
+        }
+        return null;
+    }
+
+    public static boolean checkConsistency(List<MeetingVariable> variables, Set<DateConstraint> constraints) {
+        boolean isValid = false;
+        for (DateConstraint constraint : constraints) {
+            LocalDate leftDate = variables.get(constraint.L_VAL).getAssignment();
+            LocalDate rightDate = (constraint.arity() == 1) ? ((UnaryDateConstraint) constraint).R_VAL
+                    : variables.get(((BinaryDateConstraint) constraint).R_VAL).getAssignment();
+            isValid = false;
+            if (leftDate == null || rightDate == null) {
+                isValid = true;
+                continue;
+            }
+            switch (constraint.OP) {
+            case "==":
+                if (leftDate.isEqual(rightDate))
+                    isValid = true;
+                break;
+            case "!=":
+                if (!leftDate.isEqual(rightDate))
+                    isValid = true;
+                break;
+            case ">":
+                if (leftDate.isAfter(rightDate))
+                    isValid = true;
+                break;
+            case "<":
+                if (leftDate.isBefore(rightDate))
+                    isValid = true;
+                break;
+            case ">=":
+                if (leftDate.isAfter(rightDate) || leftDate.isEqual(rightDate))
+                    isValid = true;
+                break;
+            case "<=":
+                if (leftDate.isBefore(rightDate) || leftDate.isEqual(rightDate))
+                    isValid = true;
+                break;
+            }
+            if (!isValid) {
+                return false;
+            }
+        }
+        return isValid;
+    }
+
+    /** Private Classes */
+    private static class MeetingVariable {
+        List<LocalDate> domain;
+        LocalDate assignment;
+
+        MeetingVariable(LocalDate startDate, LocalDate endDate) {
+            this.domain = populateDomain(new ArrayList<LocalDate>(), startDate, endDate);
+        }
+
+        private List<LocalDate> populateDomain(List<LocalDate> domain, LocalDate startDate, LocalDate endDate) {
+            for (LocalDate date = startDate; !date.isAfter(endDate); date = date.plusDays(1)) {
+                domain.add(date);
+            }
+            return domain;
+        }
+
+        public LocalDate getAssignment() {
+            return this.assignment;
+        }
+
+        public void setAssignment(LocalDate date) {
+            this.assignment = date;
+        }
+
+        public void removeAssignment() {
+            this.assignment = null;
+        }
+
     }
 
 }
